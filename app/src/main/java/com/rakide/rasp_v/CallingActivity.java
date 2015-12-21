d6995fc87e6b5bebb64d0d1ffaca6cdb6640adcf
nindyasaridu <nindyasari13@mhs.if.its.ac.id>
2015-12-21 18:12:15 +0700

    AAdding callingActivity, incomingActivity, and circle button

--- app/src/main/java/com/rakide/rasp_v/CallingActivity.java
+++ app/src/main/java/com/rakide/rasp_v/CallingActivity.java
@@ -1,27 +1,30 @@
 package com.rakide.rasp_v;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class CallingActivity extends AppCompatActivity {
+    private ImageButton btnEndCallDialling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnEndCallDialling = (ImageButton)findViewById(R.id.btnEndCallDialling);
+
+        btnEndCallDialling.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(CallingActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
     }
--- app/src/main/java/com/rakide/rasp_v/IncomingActivity.java
+++ app/src/main/java/com/rakide/rasp_v/IncomingActivity.java
@@ -1,29 +1,35 @@
 package com.rakide.rasp_v;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class IncomingActivity extends AppCompatActivity {
+    private ImageButton btnUpCallDialling;
+    private ImageButton btnEndCallDialling;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incoming);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnUpCallDialling = (ImageButton) findViewById(R.id.btnUpCallDialling);
+        btnEndCallDialling = (ImageButton) findViewById(R.id.btnEndCallDialling);
+
+        btnUpCallDialling.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+//                Toast.makeText(incomingActivity.this, "", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(IncomingActivity.this, CallingActivity.class);
+                startActivity(intent);
+                finish();
             }
         });
-    }
 
+    }
 }
