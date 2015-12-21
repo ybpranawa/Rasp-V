b5f12fa43684b8a18a0d7e76d90a378004b4bdca
Fany Agriansyah <fanyagriansyah@gmail.com>
2015-12-21 18:00:59 +0700

    Update dial, dialler berfungsi

--- app/src/main/java/com/rakide/rasp_v/fragment/DialFragment.java
+++ app/src/main/java/com/rakide/rasp_v/fragment/DialFragment.java
@@ -1,16 +1,28 @@
 package com.rakide.rasp_v.fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.rakide.rasp_v.DiallingActivity;
 import com.rakide.rasp_v.R;
 
 public class DialFragment extends Fragment {
-
+    private String allNumber;
+    private String last = "";
+    private Button  btn1, btn2, btn3,
+                    btn4, btn5, btn6,
+                    btn7, btn8, btn9,
+                    btnCall, btn0, btnC;
+    private EditText etCallerID;
 
     public DialFragment() {
         // Required empty public constructor
@@ -21,8 +33,145 @@ public class DialFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dial, container, false);
+        View view =  inflater.inflate(R.layout.fragment_dial, container, false);
+        btn0 = (Button)view.findViewById(R.id.btn0);
+        btn1 = (Button)view.findViewById(R.id.btn1);
+        btn2 = (Button)view.findViewById(R.id.btn2);
+        btn3 = (Button)view.findViewById(R.id.btn3);
+        btn4 = (Button)view.findViewById(R.id.btn4);
+        btn5 = (Button)view.findViewById(R.id.btn5);
+        btn6 = (Button)view.findViewById(R.id.btn6);
+        btn7 = (Button)view.findViewById(R.id.btn7);
+        btn8 = (Button)view.findViewById(R.id.btn8);
+        btn9 = (Button)view.findViewById(R.id.btn9);
+        btnC = (Button)view.findViewById(R.id.btnC);
+        btnCall = (Button)view.findViewById(R.id.btnCall);
+
+        etCallerID = (EditText)view.findViewById(R.id.etCallerID);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "0";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "1";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "2";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "3";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "4";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "5";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "6";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "7";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "8";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                last = "9";
+                allNumber = etCallerID.getText().toString();
+                allNumber+=last;
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                allNumber = etCallerID.getText().toString();
+                allNumber = deleteLast(allNumber);
+                etCallerID.setText(allNumber);
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DiallingActivity.class);
+                intent.putExtra(DiallingActivity.recvUsername,allNumber);
+                startActivityForResult(intent, 0);
+            }
+        });
+        return view;
     }
-
-
+    public String deleteLast(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        return s.substring(0, s.length()-1);
+    }
 }
