1f215579d1c3f9f6f3350d46deadfc32c6f96776
Fany Agriansyah <fanyagriansyah@gmail.com>
2016-01-01 19:35:51 +0700

    Bisa calling tapi suara tidak tertransfer

--- app/src/main/java/com/rakide/rasp_v/LoginActivity.java
+++ app/src/main/java/com/rakide/rasp_v/LoginActivity.java
@@ -2,15 +2,21 @@ package com.rakide.rasp_v;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.net.sip.*;
+
+import com.rakide.rasp_v.object.IncomingCallReceiver;
 
 import java.text.ParseException;
 
@@ -20,13 +26,22 @@ public class LoginActivity extends AppCompatActivity {
     private Button btnLogin;
     private TextView tvLoginRegister;
 
+    public String domain;
     public String username;
     public String password;
     public static String recvUsername = null;
-    public SipManager mSipManager = null;
-    public SipProfile mSipProfile = null;
-    public String domain;
 
+    public String sipAddress = null;
+
+    public SipManager manager = null;
+    public SipProfile me = null;
+    public SipAudioCall call = null;
+    public IncomingCallReceiver callReceiver;
+
+    private static final int CALL_ADDRESS = 1;
+    private static final int SET_AUTH_INFO = 2;
+    private static final int UPDATE_SETTINGS_DIALOG = 3;
+    private static final int HANG_UP = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,51 +54,33 @@ public class LoginActivity extends AppCompatActivity {
         etLoginPassword = (EditText)findViewById(R.id.etLoginPassword);
 
         //username = getIntent().getStringExtra(recvUsername);
-        username = "fany";
+
         domain = "10.151.12.205";
+        username = "fany";
+        password = "pwd_fany";
         //etLoginUsername.setText(username);
         //password = etLoginPassword.getText().toString();
-        password="pwd_fany";
 
+        // Set up the intent filter.  This will be used to fire an
+        // IncomingCallReceiver when someone calls the SIP address used by this
+        // application.
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("android.SipDemo.INCOMING_CALL");
+        callReceiver = new IncomingCallReceiver();
+        this.registerReceiver(callReceiver, filter);
 
+        // "Push to talk" can be a serious pain when the screen keeps turning off.
+        // Let's prevent that.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        initializeManager();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(SipManager.isVoipSupported(LoginActivity.this)) {
                     //Toast.makeText(LoginActivity.this, "support", Toast.LENGTH_SHORT).show();
-                    if(mSipManager == null) {
-                        mSipManager = SipManager.newInstance(LoginActivity.this);
-                    }
-
-                    try {
-                        SipProfile.Builder builder = new SipProfile.Builder(username, domain);
-                        builder.setPassword(password);
-                        mSipProfile = builder.build();
-
-                        /*Intent intent = new Intent();
-                        intent.setAction("com.rakide.rasp_v.MainActivity");
-                        PendingIntent pendingIntent = PendingIntent.getBroadcast(LoginActivity.this, 0, intent, Intent.FILL_IN_DATA);
-                        mSipManager.open(mSipProfile, pendingIntent, null);*/
-
-                        mSipManager.setRegistrationListener(mSipProfile.getUriString(), new SipRegistrationListener() {
-
-                            public void onRegistering(String localProfileUri) {
-                                Toast.makeText(LoginActivity.this, "Registering...", Toast.LENGTH_SHORT).show();
-                            }
-
-                            public void onRegistrationDone(String localProfileUri, long expiryTime) {
-                                Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
-                            }
-
-                            public void onRegistrationFailed(String localProfileUri, int errorCode, String errorMessage) {
-                                Toast.makeText(LoginActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } catch (SipException e) {
-                        Toast.makeText(LoginActivity.this, "Connection Error", Toast.LENGTH_SHORT).show();
-                    } catch (ParseException pe){
-                        Toast.makeText(LoginActivity.this, "Connection Error", Toast.LENGTH_SHORT).show();
-                    }
+                    EditText textField = (EditText)findViewById(R.id.etLoginUsername);
+                    sipAddress = textField.getText().toString();
+                    initiateCall();
                 }
                 else
                     Toast.makeText(LoginActivity.this, "Tidak Support", Toast.LENGTH_SHORT).show();
@@ -98,42 +95,175 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    /*public void initializemanager()
-    {
-        if(mSipManager == null) {
-            mSipManager = SipManager.newInstance(LoginActivity.this);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // When we get back from the preference setting Activity, assume
+        // settings have changed, and re-login with new auth info.
+        initializeManager();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (call != null) {
+            call.close();
         }
+
+        closeLocalProfile();
+
+        if (callReceiver != null) {
+            this.unregisterReceiver(callReceiver);
+        }
+    }
+
+    public void initializeManager() {
+        if(manager == null) {
+            manager = SipManager.newInstance(this);
+        }
+
+        initializeLocalProfile();
+    }
+
+    /**
+     * Logs you into your SIP provider, registering this device as the location to
+     * send SIP calls to for your SIP address.
+     */
+    public void initializeLocalProfile() {
+        if (manager == null) {
+            return;
+        }
+
+        if (me != null) {
+            closeLocalProfile();
+        }
+
+//        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+//        String username = prefs.getString("namePref", "");
+//        String domain = prefs.getString("domainPref", "");
+//        String password = prefs.getString("passPref", "");
 
         try {
             SipProfile.Builder builder = new SipProfile.Builder(username, domain);
             builder.setPassword(password);
-            mSipProfile = builder.build();
+            me = builder.build();
 
-            Intent intent = new Intent();
-            intent.setAction("com.rakide.rasp_v.MainActivity");
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(LoginActivity.this, 0, intent, Intent.FILL_IN_DATA);
-            mSipManager.open(mSipProfile, pendingIntent, null);
+            Intent i = new Intent();
+            i.setAction("android.SipDemo.INCOMING_CALL");
+            PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, Intent.FILL_IN_DATA);
+            manager.open(me, pi, null);
 
 
-            mSipManager.setRegistrationListener(mSipProfile.getUriString(), new SipRegistrationListener() {
+            // This listener must be added AFTER manager.open is called,
+            // Otherwise the methods aren't guaranteed to fire.
 
+            manager.setRegistrationListener(me.getUriString(), new SipRegistrationListener() {
                 public void onRegistering(String localProfileUri) {
-                    Toast.makeText(LoginActivity.this, "Registering...", Toast.LENGTH_SHORT).show();
+                    updateStatus("Registering with SIP Server...");
                 }
 
                 public void onRegistrationDone(String localProfileUri, long expiryTime) {
-                    Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                    updateStatus("Ready");
                 }
 
-                public void onRegistrationFailed(String localProfileUri, int errorCode, String errorMessage) {
-                    Toast.makeText(LoginActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                public void onRegistrationFailed(String localProfileUri, int errorCode,
+                                                 String errorMessage) {
+                    updateStatus("Registration failed.  Please check settings.");
                 }
             });
-        } catch (SipException e) {
+        } catch (ParseException pe) {
+            updateStatus("Connection Error.");
+        } catch (SipException se) {
+            updateStatus("Connection error.");
+        }
+    }
 
-        } catch (ParseException pe){
+    /**
+     * Closes out your local profile, freeing associated objects into memory
+     * and unregistering your device from the server.
+     */
+    public void closeLocalProfile() {
+        if (manager == null) {
+            return;
+        }
+        try {
+            if (me != null) {
+                manager.close(me.getUriString());
+            }
+        } catch (Exception ee) {
+            Log.d("WalkActivity/onDestroy", "Failed to close local profile.", ee);
+        }
+    }
+
+    /**
+     * Make an outgoing call.
+     */
+    public void initiateCall() {
+
+        updateStatus(sipAddress);
+
+        try {
+            SipAudioCall.Listener listener = new SipAudioCall.Listener() {
+                // Much of the client's interaction with the SIP Stack will
+                // happen via listeners.  Even making an outgoing call, don't
+                // forget to set up a listener to set things up once the call is established.
+                @Override
+                public void onCallEstablished(SipAudioCall call) {
+                    call.startAudio();
+                    call.setSpeakerMode(true);
+//                    call.toggleMute();
+                    updateStatus(call);
+                }
+
+                @Override
+                public void onCallEnded(SipAudioCall call) {
+                    updateStatus("Ready.");
+                }
+            };
+
+            call = manager.makeAudioCall(me.getUriString(), sipAddress, listener, 30);
 
         }
-    }*/
+        catch (Exception e) {
+            Log.i("WalkieTalkieActivity/InitiateCall", "Error when trying to close manager.", e);
+            if (me != null) {
+                try {
+                    manager.close(me.getUriString());
+                } catch (Exception ee) {
+                    Log.i("WalkieTalkieActivity/InitiateCall",
+                            "Error when trying to close manager.", ee);
+                    ee.printStackTrace();
+                }
+            }
+            if (call != null) {
+                call.close();
+            }
+        }
+    }
 
+    /**
+     * Updates the status box at the top of the UI with a messege of your choice.
+     * @param status The String to display in the status box.
+     */
+    public void updateStatus(final String status) {
+        // Be a good citizen.  Make sure UI changes fire on the UI thread.
+        this.runOnUiThread(new Runnable() {
+            public void run() {
+                TextView labelView = (TextView) findViewById(R.id.tvStatus);
+                labelView.setText(status);
+            }
+        });
+    }
+
+    /**
+     * Updates the status box with the SIP address of the current call.
+     * @param call The current, active call.
+     */
+    public void updateStatus(SipAudioCall call) {
+        String useName = call.getPeerProfile().getDisplayName();
+        if(useName == null) {
+            useName = call.getPeerProfile().getUserName();
+        }
+        updateStatus(useName + "@" + call.getPeerProfile().getSipDomain());
+    }
 }
